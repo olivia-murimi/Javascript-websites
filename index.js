ec147daa49f58df6836ce5ef19dd8601aca1168b
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-19 14:06:40 +0300

    adding  a variable

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 //window.alert(`This is an alert!`);
 //window.alert(`I like pizza!`);
 document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = "I like pizza";g
+document.getElementById("myP").textContent = "I like pizza";
 //This is a comment
 /*This is a comment*/
+let x;
+let y;
