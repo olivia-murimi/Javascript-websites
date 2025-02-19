84c9429934028c01c6d7ab0347e623880502c4b0
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-19 14:40:57 +0300

    text content

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.getElementById("myH1").textContent = "Hello";
 document.getElementById("myP").textContent = "I like pizza";
 //This is a comment
 /*This is a comment*/
-let age = 25;
+/*let age = 25;
 let price = 10.99;
 let gpa = 2.1;
 console.log(typeof gpa);
@@ -23,4 +23,10 @@ let email = "murimiwaithira@gmail.com";
 console.log(`Your email is ${email}`);
 let online = true;
 console.log(typeof online);
-console.log(`Bro is online: ${online}`);
+console.log(`Bro is online: ${online}`);*/
+let fullName = "Olivia murimi";
+let age = "25";
+let student = true;
+document.getElementById("p1").textContent =fullName;
+document.getElementById("p2").textContent =fullName;
+document.getElementById("p3").textContent =fullName;
