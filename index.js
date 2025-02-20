7c333b2e7f42c7bb780e055add1f252ffafe8137
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-20 23:52:51 +0300

    operations

--- index.js
+++ index.js
@@ -36,5 +36,10 @@ let students = 30;
 //students = students * 2;
 //students = students / 2;
 //students = students ** 2;
-students = students % 2;
+//students = students % 2;
+
+students +=1;
+students-=1;
+students *=2;
+students /=2;
 console.log(students);
