74754ca37642a024ef151943ab3b55de7c9cc665
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-20 23:46:31 +0300

    arithmetic

--- index.js
+++ index.js
@@ -31,4 +31,7 @@ document.getElementById("p1").textContent =`Your name is ${fullName}`;
 document.getElementById("p2").textContent =`You are ${age} years old`;
 document.getElementById("p3").textContent =`Enrolled: ${isStudent}`;
 let students = 30;
+students = students + 1;
+students = students - 1;
+students = students * 2;
 console.log(students);
