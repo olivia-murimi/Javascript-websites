cdd2a8270bc36895d3e92e9541af6a5ef5e6670b
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-20 23:45:09 +0300

    arithmetic operation

--- index.js
+++ index.js
@@ -30,3 +30,5 @@ let student = true;
 document.getElementById("p1").textContent =`Your name is ${fullName}`;
 document.getElementById("p2").textContent =`You are ${age} years old`;
 document.getElementById("p3").textContent =`Enrolled: ${isStudent}`;
+let students = 30;
+console.log(students);
