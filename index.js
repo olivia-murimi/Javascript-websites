936110b2aa90d816c4a6bee579d79c76e66cd63c
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-28 13:33:38 +0300

    calculating the area

--- index.js
+++ index.js
@@ -77,5 +77,7 @@ console.log(z, typeof z);*/
 let pi = 3.14159;
 let radius;
 let circumference;
-radius = window.prompt("Enter the radius of a circle");
+radius = window.prompt('Enter the radius of a circle');
 radius = Number(radius);
+circumference = 2 * pi * radius;
+ console.log(circumference);
