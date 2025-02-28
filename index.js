67ed69eb69a9a194a0d7a36db429e41be250cf78
Olivia Waithira <oliviawaithira7@gmail.com>
2025-02-28 13:46:50 +0300

    constants

--- index.js
+++ index.js
@@ -77,7 +77,9 @@ console.log(z, typeof z);*/
 const PI = 3.14159;
 let radius;
 let circumference;
-radius = window.prompt('Enter the radius of a circle');
-radius = Number(radius);
+
 circumference = 2 * pi * radius;
- console.log(circumference);
+ document.getElementById("mySubmit").onclick = function(){
+  document.getElementById("myText").value;
+  radius = Number(radius);
+ }
