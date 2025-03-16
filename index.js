bffef4885622599a89ad21be78e7a516b08e115c
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-16 15:51:05 +0300

    javascript

--- index.js
+++ index.js
@@ -107,11 +107,20 @@ console.log(z, typeof z);*/
 //console.log(randomNum);
 const myButton = document.getElementById("myButton");
 const label1= document.getElementById("label1");
+const label2= document.getElementById("label2");
+const label3= document.getElementById("label3");
 const min = 1;
 const max = 6;
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
 
 myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min;
-    myLabel.textContent = randomNum;
+   randomNum1 = Math.floor(Math.random() * max) + min;
+   randomNum2 = Math.floor(Math.random() * max) + min;
+   randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
 }
