ff7cdbc4187d65bb0141878de41cb1f8c64599e1
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-16 15:19:26 +0300

    countlabel

--- index.js
+++ index.js
@@ -87,3 +87,14 @@ console.log(z, typeof z);*/
  const decreaseBtn = document.getElementById("decreaseBtn");
  const resetBtn = document.getElementById("resetBtn");
  const increaseBtn = document.getElementById("increaseBtn");
+ const countLabel = document.getElementById("countLabel");
+ let count = 0;
+
+ increaseBtn.onclick = function(){
+   count++;
+   countLabel.textContent = count;
+ }
+ decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+ }
