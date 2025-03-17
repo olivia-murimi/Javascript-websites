e43af431b4c2466a83f01af6412d042a37593d6f
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-18 00:49:12 +0300

    else if

--- index.js
+++ index.js
@@ -152,21 +152,24 @@ const mySubmit = document.getElementById("mySubmit");
 const resultElement = document.getElementById("resultElement");
 let age;
 
-mySubmit.onclick= function{
-   age = 
+mySubmit.onclick= function(){
+   age = myText.value;
+   ge = Number(age);
   if(age >= 100){
-     console.log("You are Too OLD to enter this site");
+     resultElement.textContent = `You are Too OLD to enter this site`;
   }
   else if(age == 0){
-   console.log("You can't enter.You were just born");
+   resultElement.textContent = `You can't enter.You were just born`;
   }
   else if(age >= 18){
-   console.log("You are old enough to enter this site");
+   resultElement.textContent = `You are old enough to enter this site`;
   }
   else if(age < 0){
-   console.log("Your age can't be below 0");
+   
+   resultElement.textContent = `Your age can't be below 0`;
   }
   else{
-   console.log("You must be 18+ to enter this site");
+   
+   resultElement.textContent = `You must be 18+ to enter this site`;
   }
 }
