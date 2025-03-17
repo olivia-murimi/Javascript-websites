e7a12091f9560acf315ee29de0ae10daccb9a2e4
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-18 00:06:29 +0300

    nested loops

--- index.js
+++ index.js
@@ -135,8 +135,14 @@ else{
 }
 let age =25;
 let hasLicense = false;
-if(ge >= 16){
+if(age >= 16){
    console.log("You are old enough to drive");
+   if(hasLicense){
+      console.log("You have your license!");
+   } 
+   else{
+      console.log("You do not have your license yet!");
+   }
 }
 else{
    console.log("You must be 16+ to have a license");
