6b745ffc214406d125be0e6691a39c6494f4ff1c
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-18 00:02:57 +0300

    if statements

--- index.js
+++ index.js
@@ -133,3 +133,11 @@ if(age >= 18){
 else{
    console.log("You must be 18+ to enter this site");
 }
+let age =25;
+let hasLicense = false;
+if(ge >= 16){
+   console.log("You are old enough to drive");
+}
+else{
+   console.log("You must be 16+ to have a license");
+}
