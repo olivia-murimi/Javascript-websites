1e03406eabb98684689330ef9a67963879e31299
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-18 00:40:14 +0300

    else if

--- index.js
+++ index.js
@@ -147,13 +147,26 @@ if(age >= 16){
 else{
    console.log("You must be 16+ to have a license");
 }*/
-let age = 18;
- if(age >= 18){
-   console.logf("You are old enough to enter this site");
- }
- else if(age < 0){
-  console.log("Your age can't be below zero");
- }
- else{
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick= function{
+   age = 
+  if(age >= 100){
+     console.log("You are Too OLD to enter this site");
+  }
+  else if(age == 0){
+   console.log("You can't enter.You were just born");
+  }
+  else if(age >= 18){
+   console.log("You are old enough to enter this site");
+  }
+  else if(age < 0){
+   console.log("Your age can't be below 0");
+  }
+  else{
    console.log("You must be 18+ to enter this site");
- }
+  }
+}
