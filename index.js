32d2c7ddb1df0b1bd820a5d455484b5cd33e8706
Olivia Waithira <oliviawaithira7@gmail.com>
2025-03-17 23:56:46 +0300

    else

--- index.js
+++ index.js
@@ -126,6 +126,10 @@ myButton.onclick = function(){
 }*/
 //If statements= if a condition is true,m execute some code if not do something else.
 let age = 25;
+
 if(age >= 18){
- console.log("You are old enough to enter")
+ console.log("You are old enough to enter this site");
+}
+else{
+   console.log("You must be 18+ to enter this site");
 }
